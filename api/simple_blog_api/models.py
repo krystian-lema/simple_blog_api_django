ae221ad957dc4f36000a372ea3d4f5623e5d7793
Krystian Lema <krystian.lema@gmail.com>
2019-03-07 18:27:49 +0100

    Define models.

--- api/simple_blog_api/models.py
+++ api/simple_blog_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    firstname = models.CharField(max_length=40, null=False)
+    lastname = models.CharField(max_length=40, null=False)
+    bio = models.CharField(max_length=200, null=False, blank=True)
+
+class Article(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    content = models.CharField(max_length=255, null=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    author_name = models.CharField(max_length=20, null=False)
+    content = models.CharField(max_length=200, null=False)
+    article= models.ForeignKey(Article,related_name='comments', on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    article= models.ForeignKey(Article, related_name='tags', on_delete=models.CASCADE)
--- api/simple_blog_api/urls.py
+++ api/simple_blog_api/urls.py
@@ -3,6 +3,11 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
+# router.register('authors', views.ListAuthorView)
+# router.register('articles', views.ListArticleView)
+# router.register('articles_include_all', views.ListArticleIncludeAllView)
+# router.register('comments', views.ListCommentView)
+# router.register('tags', views.ListTagView)
 
 urlpatterns = [
     path('', include(router.urls))
