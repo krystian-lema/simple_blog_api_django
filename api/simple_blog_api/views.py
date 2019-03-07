6c9f4679b705747b0dc087a2210b7665c50c8bbd
Krystian Lema <krystian.lema@gmail.com>
2019-03-07 18:32:11 +0100

    Add views for serializers.

--- api/simple_blog_api/views.py
+++ api/simple_blog_api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Author, Article, Comment, Tag
+from .serializers import AuthorSerializer, ArticleIncludeAllSerializer, ArticleSerializer, CommentSerializer, TagSerializer
 
-# Create your views here.
+class ListAuthorView(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class ListArticleView(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ListCommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class ListTagView(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
